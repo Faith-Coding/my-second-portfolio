03143514d770c6ba6579f004301a3a28aaff22aa
Akhigbe1010 <Akhigbefaithbright@gmail.com>
2023-03-17 14:03:05 +0100

    JS was added

--- src/app.js
+++ src/app.js
@@ -1,20 +1,41 @@
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+/*================toggle icon navba=======*/
 
-window.onscroll  = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset =sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+/*let menuIcon = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navbar');
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-               links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
+menuIcon.onclick = () => {
+  menuIcon.classList.add("fa-x");
+  navBar.classList.add('active');
+}*/
+let menuIcon = document.querySelector("#menu-icon");
+ 
+menuIcon.onclick = function() {
+  let navBar = document.querySelector(".nav-bar");
+  navBar.classList.toggle("active")
+}
+/*================srcol selections active link=======*/
 
-        };
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
-    });
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  /*================sticky navbar=======*/
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
 };
